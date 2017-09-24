140350c4709f94992b12ece5211bc767f9094bd2
Neil Patel <neilp980@yahoo.com>
2017-09-24 00:54:56 -0400

    added initial loading bars

--- app/src/main/java/com/vptech/fitness/app/MainActivity.java
+++ app/src/main/java/com/vptech/fitness/app/MainActivity.java
@@ -4,14 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    ProgressBar progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        progress = findViewById(R.id.progress_bar1);
+        progress.setProgress(50);
     }
 
 
